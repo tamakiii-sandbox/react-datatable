76fa306d0724e22f6a9958cd9f22a735682831f4
tamakiii <tamakiii@users.noreply.github.com>
2019-02-01 17:47:37 +0900

    " -> '

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: [
-    path.resolve(__dirname, "src/index.tsx"),
+    path.resolve(__dirname, 'src/index.tsx')
   ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        loader: "ts-loader"
+        loader: 'ts-loader'
       }
     ]
   }
