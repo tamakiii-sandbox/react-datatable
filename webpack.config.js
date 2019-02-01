df235a235227d4a00aa847e42a5af864dbe24c31
tamakiii <tamakiii@users.noreply.github.com>
2019-02-01 17:26:12 +0900

    add script dev

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, "src/index.tsx"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
