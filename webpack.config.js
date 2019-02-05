131268231fd5f465d36717ab50da9da0a3bfc1ee
tamakiii <tamakiii@users.noreply.github.com>
2019-02-05 23:04:57 +0900

    fix .css$ loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader'
+      },
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader']
       }
     ]
   }
