4b6a05212fa466b5d4ed22e803ca8bc64247f28f
tamakiii <tamakiii@users.noreply.github.com>
2019-02-05 23:54:27 +0900

    using props

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,27 @@ import * as ReactDOM from "react-dom";
 import 'normalize.css';
 import './app.css';
 
+interface Column {
+  id: string;
+  label: string;
+};
+
+// interface Row {
+//   id: any;
+//   name: string;
+//   birthday: string;
+//   gender: string;
+//   group: string;
+//   [key: string]: string;
+// }
+
+interface Table<R> {
+  head: Array<Column>;
+  body: Array<R>;
+};
+
 interface Props {
-  title: string
+  table: Table<any>;
 };
 
 const App = (props: Props) => {
@@ -12,48 +31,82 @@ const App = (props: Props) => {
     <table>
       <thead>
         <tr>
-          <th><input type="checkbox" name="id" /></th>
-          <th>Name</th>
-          <th>Birthday</th>
-          <th>Gender</th>
-          <th>Group</th>
+          {props.table.head.map((head) => {
+            return (
+              <th>{head.label}</th>
+            );
+          })}
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td><input type="checkbox" name="id" /></td>
-          <td>Tom Jenkinson</td>
-          <td>Jan 17, 1975</td>
-          <td>Male</td>
-          <td>Squarepusher</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="id" /></td>
-          <td>Thomas Bangalter</td>
-          <td>Jan 3, 1975</td>
-          <td>Unknown</td>
-          <td>Daft Punk</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="id" /></td>
-          <td>Guy-Manuel de Homem-Christo</td>
-          <td>Feb 8, 1974</td>
-          <td>Unknown</td>
-          <td>Daft Punk</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" name="id" /></td>
-          <td>Midori Hara</td>
-          <td>Feb 2, 1966</td>
-          <td>Female</td>
-          <td>Spank Happy</td>
-        </tr>
+        {props.table.body.map((row) => {
+          return (
+            <tr>
+              {props.table.head.map((head) => {
+                return (
+                  <td>{row[head.id]}</td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
 }
 
+const table = {
+  head: [
+    {
+      id: "name",
+      label: "Name",
+    },
+    {
+      id: "birthday",
+      label: "Birthday",
+    },
+    {
+      id: "gender",
+      label: "Gender",
+    },
+    {
+      id: "group",
+      label: "Group",
+    }
+  ],
+  body: [
+    {
+      id: 1,
+      name: "Tom Jenkinson",
+      birthday: "Jan 17, 1975",
+      gender: "Male",
+      group: "Squarepusher",
+    },
+    {
+      id: 2,
+      name: "Thomas Bangalter",
+      birthday: "Jan 3, 1975",
+      gender: "Unknown",
+      group: "Daft Punk",
+    },
+    {
+      id: 3,
+      name: "Guy-Manuel de Homem-Christo",
+      birthday: "Feb 8, 1974",
+      gender: "Unknown",
+      group: "Daft Punk",
+    },
+    {
+      id: 4,
+      name: "Midori Hara",
+      birthday: "Feb 2, 1966",
+      gender: "Female",
+      group: "Spank Happy",
+    }
+  ]
+}
+
 ReactDOM.render(
-  <App title="hello world" />,
+  <App table={table} />,
   document.querySelector("#app")
 );
